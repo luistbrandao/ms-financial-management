45e259b9d35664500269eb7651e4619803a49bf2
luis <luisotbrandao@gmail.com>
2021-11-22 15:41:49 -0300

    first commit

---
+++ src/main/java/br/com/project/msfinancial/MsFinancialApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.msfinancial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFinancialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFinancialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/project/msfinancial/MsFinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.msfinancial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
